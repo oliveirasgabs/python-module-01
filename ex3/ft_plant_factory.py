3a5a383c6417d00ce3f146275af9e03f176467ee
Gabrielly <oliveiragabrielly.dev@gmail.com>
2026-03-23 00:06:42 -0300

    Fazendo exercicio 3

---
+++ ex3/ft_plant_factory.py
@@ -0,0 +1,32 @@
+class Plant:
+    def __init__(self, name: str, height: float, days_age: int) -> None:
+        self.name = name
+        self.height = height
+        self.days_age = days_age
+        print(f"Created: ", end="")
+        self.show()
+
+    def show(self) -> None:
+        print(f"{self.name}: {round(self.height, 1)}cm, "
+              f"{self.days_age} days old")
+
+    def grow(self, growth_rate: int) -> None:
+        self.height += growth_rate
+
+    def age(self) -> None:
+        self.days_age += 1
+
+
+def main() -> None:
+    print("=== Plant Factory Output ===")
+    garden: list[Plant] = [
+        Plant("Rose", 25.0, 30),
+        Plant("Oak", 200.0, 365),
+        Plant("Cactus", 5.0, 90),
+        Plant("Sunflower", 80.0, 45),
+        Plant("Fern", 5.0, 120)
+    ]
+
+
+if __name__ == "__main__":
+    main()
